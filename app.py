129a1beb43d847dc45ee9a74af933eed3412f278
katsudoon1 <todayoshi8@gmail.com>
2023-08-28 18:26:54 +0900

    Fix Heroku deployment issues

--- app.py
+++ app.py
@@ -44,5 +44,7 @@ def chat():
     session_messages[session_id].append({"role": "assistant", "content": chat_output})
     return jsonify({"chat_output": chat_output})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
